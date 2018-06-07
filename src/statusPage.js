2c9dc6b9d548cae633a492d7db23ae25f6983156
andormade <hngrhorace@gmail.com>
2018-06-08 00:48:18 +0200

    add proper status page

--- src/statusPage.js
+++ src/statusPage.js
@@ -1,5 +1,17 @@
 const statusPage = function(req, res) {
-	res.status(200).send('Minden OK');
+	const body = [
+		`Processor architecture: ${process.arch}`,
+		`Platform: ${process.platform}`,
+		`Uptime: ${process.uptime()}`,
+		`CPU usage (user): ${process.cpuUsage().user}`,
+		`CPU usage (system): ${process.cpuUsage().system}`,
+		`Memory usage (rss): ${process.memoryUsage().rss}`,
+		`Memory usage (heapTotal): ${process.memoryUsage().heapTotal}`,
+		`Memory usage (heapUsed): ${process.memoryUsage().heapUsed}`,
+		`Memory usage (external): ${process.memoryUsage().external}`
+	].join('<br />');
+
+	res.status(200).send(body);
 };
 
 module.exports = { statusPage };
