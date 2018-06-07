8463ea591ed0caa1ab9aa4694155d8bd969273d2
andormade <hngrhorace@gmail.com>
2018-06-08 00:29:33 +0200

    add concurrency

--- src/app.js
+++ src/app.js
@@ -13,87 +13,112 @@ const DeezerApi = require('./apiHelpers/deezer');
 const conversationRouter = require('./conversationRouter').router;
 const StatusPage = require('./statusPage');
 const app = express();
+const throng = require('throng');
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-app.set('port', process.env.PORT || 5000);
-app.set('view engine', 'ejs');
-app.use(bodyParser.json({ verify: FacebookAuth.verifyRequestSignature }));
-app.get('/', StatusPage.statusPage);
-app.get('/spotify-login', SpotifyApi.login);
-app.get('/spotify-callback', async function(req, res) {
-	const { accessToken, psid } = await SpotifyApi.getAccessToken(req, res);
-	const newContext = await ContextProvider.set(psid, {
-		spotifyAccessToken: accessToken,
-		topArtists: await SpotifyApi.getTopArtists(accessToken),
-		topGenres: ['pinarock']
-	});
+throng(
+	{
+		workers: WORKERS,
+		lifetime: Infinity
+	},
+	function() {
+		app.set('port', process.env.PORT || 5000);
+		app.set('view engine', 'ejs');
+		app.use(
+			bodyParser.json({ verify: FacebookAuth.verifyRequestSignature })
+		);
+		app.get('/', StatusPage.statusPage);
+		app.get('/spotify-login', SpotifyApi.login);
+		app.get('/spotify-callback', async function(req, res) {
+			const { accessToken, psid } = await SpotifyApi.getAccessToken(
+				req,
+				res
+			);
+			const newContext = await ContextProvider.set(psid, {
+				spotifyAccessToken: accessToken,
+				topArtists: await SpotifyApi.getTopArtists(accessToken),
+				topGenres: ['pinarock']
+			});
 
-	conversationRouter('/stream-provider-auth/data-received', newContext);
-});
-app.get('/deezer-login', DeezerApi.login);
-app.get('/deezer-callback', async function(req, res) {
-	const { accessToken, psid } = await DeezerApi.getAccessToken(req, res);
-	const newContext = await ContextProvider.set(psid, {
-		deezerAccesToken: accessToken,
-		topArtists: await DeezerApi.getTopArtists(accessToken),
-		topGenres: ['pinarock']
-	});
+			conversationRouter(
+				'/stream-provider-auth/data-received',
+				newContext
+			);
+		});
+		app.get('/deezer-login', DeezerApi.login);
+		app.get('/deezer-callback', async function(req, res) {
+			const { accessToken, psid } = await DeezerApi.getAccessToken(
+				req,
+				res
+			);
+			const newContext = await ContextProvider.set(psid, {
+				deezerAccesToken: accessToken,
+				topArtists: await DeezerApi.getTopArtists(accessToken),
+				topGenres: ['pinarock']
+			});
 
-	conversationRouter('/stream-provider-auth/data-received', newContext);
-});
-app.get('/webhook', FacebookAuth.validateWebhook);
-app.get('/authorize', FacebookAuth.authorize);
+			conversationRouter(
+				'/stream-provider-auth/data-received',
+				newContext
+			);
+		});
+		app.get('/webhook', FacebookAuth.validateWebhook);
+		app.get('/authorize', FacebookAuth.authorize);
 
-app.post('/webhook', function(req, res) {
-	const data = req.body;
+		app.post('/webhook', function(req, res) {
+			const data = req.body;
 
-	// Make sure this is a page subscription
-	if (data.object !== 'page') {
-		return res.sendStatus(404);
-	}
+			// Make sure this is a page subscription
+			if (data.object !== 'page') {
+				return res.sendStatus(404);
+			}
 
-	// Iterate over each entry
-	// There may be multiple if batched
-	data.entry.forEach(function(pageEntry) {
-		if (!pageEntry.messaging) {
-			return;
+			// Iterate over each entry
+			// There may be multiple if batched
+			data.entry.forEach(function(pageEntry) {
+				if (!pageEntry.messaging) {
+					return;
+				}
+
+				pageEntry.messaging.forEach(function(messagingEvent) {
+					if (
+						messagingEvent.message &&
+						messagingEvent.message.quick_reply
+					) {
+						receivedPostback(
+							messagingEvent.sender.id,
+							messagingEvent.message.quick_reply.payload
+						);
+					} else if (messagingEvent.postback) {
+						receivedPostback(
+							messagingEvent.sender.id,
+							messagingEvent.postback.payload
+						);
+					}
+				});
+			});
+
+			res.sendStatus(200);
+		});
+
+		async function receivedPostback(psid, payload) {
+			const context = await ContextProvider.get(psid);
+			const facebookData = await FacebookGraph.getUserInformation(psid);
+			if (!context.facebookData) {
+				await ContextProvider.set(psid, {
+					facebookData: facebookData,
+					name: facebookData.first_name
+				});
+			}
+
+			conversationRouter(payload, context);
 		}
 
-		pageEntry.messaging.forEach(function(messagingEvent) {
-			if (messagingEvent.message && messagingEvent.message.quick_reply) {
-				receivedPostback(
-					messagingEvent.sender.id,
-					messagingEvent.message.quick_reply.payload
-				);
-			} else if (messagingEvent.postback) {
-				receivedPostback(
-					messagingEvent.sender.id,
-					messagingEvent.postback.payload
-				);
-			}
+		app.listen(app.get('port'), () => {
+			console.log('Listening on port ' + app.get('port'));
 		});
-	});
 
-	res.sendStatus(200);
-});
-
-async function receivedPostback(psid, payload) {
-	const context = await ContextProvider.get(psid);
-	const facebookData = await FacebookGraph.getUserInformation(psid);
-	if (!context.facebookData) {
-		await ContextProvider.set(psid, {
-			facebookData: facebookData,
-			name: facebookData.first_name
-		});
+		console.log('Setting up messenger profile...');
+		FacebookGraph.setUpMessengerProfile();
 	}
-
-	conversationRouter(payload, context);
-}
-
-app.listen(app.get('port'), () => {
-	console.log('Listening on port ' + app.get('port'));
-});
-
-console.log('Setting up messenger profile...');
-FacebookGraph.setUpMessengerProfile();
-
-module.exports = app;
+);
