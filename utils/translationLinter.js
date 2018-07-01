9b20661868c0dc7225439c22b559a24bb9519968
andormade <hngrhorace@gmail.com>
2018-07-01 18:50:37 +0100

    update translation linter

--- utils/translationLinter.js
+++ utils/translationLinter.js
@@ -16,12 +16,24 @@ function replaceKeys(text) {
 	return text;
 }
 
+function getLineNumber(file, text) {
+	const regex = /\n/g;
+	const position = file.indexOf(text);
+	const beforePosition = file.slice(0, position);
+	const newLines = beforePosition.match(regex);
+	return newLines.length + 1;
+}
+
 function getTexts(file) {
 	const regex = /t`([a-zA-Z.,'’_?!$áÁéÉíÍóÓöÖőŐüÜ\s\+\{\}]+)`/g;
 	return (texts = file
 		.match(regex)
 		.map(text => text.substring(2))
-		.map(text => text.slice(0, -1)));
+		.map(text => text.slice(0, -1)))
+		.map(text => ({
+			line: getLineNumber(file, text),
+			text: text
+		}));
 }
 
 fs.readdirSync('./src/conversations').forEach(file => {
@@ -29,11 +41,11 @@ fs.readdirSync('./src/conversations').forEach(file => {
 
 	Object.keys(locales).forEach(locale => {
 		getTexts(content).forEach(text => {
-			if (!locales[locale][replaceKeys(text)]) {
+			if (!locales[locale][replaceKeys(text.text)]) {
 				console.log(chalk.black.bgYellow('WARN') +
-					' Missing translation:',
-					file,
-					'"' + replaceKeys(text) + '"'
+					' ' + file + ':' + text.line,
+					'Missing translation:',
+					'"' + replaceKeys(text.text) + '"'
 				);
 			}
 		});
