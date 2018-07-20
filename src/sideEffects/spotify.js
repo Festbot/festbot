46568afd23f464615dabc4904923470906542846
andormade <hngrhorace@gmail.com>
2018-07-21 01:08:50 +0200

    prettify

--- src/sideEffects/spotify.js
+++ src/sideEffects/spotify.js
@@ -1,14 +1,12 @@
 const SpotifyApi = require('../apiHelpers/spotify');
 
-const {
-	GET_SPOTIFY_ARTISTS,
-} = require('../actionTypes');
+const { GET_SPOTIFY_ARTISTS } = require('../actionTypes');
 
 const executeAction = async function({ type, payload }) {
 	switch (type) {
 		case GET_SPOTIFY_ARTISTS:
 			return await SpotifyApi.getTopArtists(payload);
 	}
-}
+};
 
 module.exports = executeAction;
